5e1b4f2109e31bf6e0cf8735b97fba7ad01d711a
dominikw1 <dominik.m.weber@gmail.com>
2022-06-24 09:18:59 +0200

    cleaning up code

--- AutoPDFOpener.py
+++ AutoPDFOpener.py
@@ -1,27 +1,24 @@
 import glob
 import os
 
-
-
-
+#grab file names
 pdfList = glob.glob("E:/.everything/books/.Currently Reading/*.pdf")
-#print(pdfList);
 
+
+#get current index
 index = 0;
 with open("index.txt", "r") as f:
-   # perform file operations
-   
    indexStr = f.read()
    index = 0
    if(len(indexStr) != 0):
        index = int(indexStr)
-
    index +=1
    index %= len(pdfList)
+
+#write updated index
 with open("index.txt", "w") as f:
-   indexstr = str(index)
-   print(index)
-   f.write(indexstr)
+   indexStr = str(index)
+   f.write(indexStr)
 
-
+#open pdf
 os.startfile(pdfList[index])
